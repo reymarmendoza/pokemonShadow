58139993f91948a2f786fbaa61e27f97c8f5ebe3
Reinaldo Omar <reymar.mendoza@gmail.com>
2022-01-16 09:41:00 -0500

    [trivia] fetch additional information to build a multiple answers menu

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const searchBtn = document.querySelector('#search')
 const form = document.querySelector('form')
 
 refreshBtn.addEventListener('click', () => {
-	const numPkm = Math.floor(Math.random() * (152 - 1) + 1)
-	const url = `${API}${numPkm}`
+	let optionsPkm = []
 
-	fetch(url)
+	fetch(`${API}${generateRdmNum()}`)
 		.then(res => res.json())
 		.then(res => {
-			console.log(res.forms[0].name)
+			optionsPkm.push(res.forms[0].name)
+
 			if (document.querySelector('.shadow')) {
 				document.querySelector('.shadow').remove()
 			}
@@ -22,9 +22,45 @@ refreshBtn.addEventListener('click', () => {
 			imgPkm.setAttribute('width', '192')
 			form.after(imgPkm)
 		})
+		// <input type='radio' class='pkmOption' name='pkmSelection' />
 		.catch(error => {
 			console.warn(error)
 		})
+
+	loadOptions()
+		.then(() => console.log('optionsPkm:', optionsPkm))
+
+	function generateRdmNum() {
+		return Math.floor(Math.random() * (152 - 1) + 1)
+	}
+
+	function loadOptions() {
+		return new Promise((resolve, reject) => {
+			for (let i = 0; i < 3; i++) {
+				fetchPkmNames()
+					// .then(res => console.log(res))
+					.then(res => {
+						optionsPkm.push(res)
+						if (i === 2) {
+							resolve()
+						}
+					})
+					.catch(err => {
+						console.log(err)
+						reject(err)
+					})
+			}
+		})
+	}
+
+	function fetchPkmNames() {
+		return new Promise((resolve, reject) => {
+			fetch(`${API}${generateRdmNum()}`)
+				.then(res => res.json())
+				.then(res => resolve(res.forms[0].name))
+				.catch(err => reject(err))
+		})
+	}
 })
 
 searchBtn.addEventListener('click', () => {
