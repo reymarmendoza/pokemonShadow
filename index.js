9a12ec6b0cf6cdc86c65dc7db906deacc7941144
Reinaldo Omar <reymar.mendoza@gmail.com>
2022-01-15 23:50:26 -0500

    [display] display grid is much better for the user, trivia game is progressing

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const API = 'https://pokeapi.co/api/v2/pokemon/'
 
+const refreshBtn = document.querySelector('#refresh')
 const searchBtn = document.querySelector('#search')
+const form = document.querySelector('form')
 
-searchBtn.addEventListener('click', () => {
-	const guess = document.querySelector('#userGuess').value
-	const form = document.querySelector('form')
-	const url = `${API}${guess}`
+refreshBtn.addEventListener('click', () => {
+	const numPkm = Math.floor(Math.random() * (152 - 1) + 1)
+	const url = `${API}${numPkm}`
 
 	fetch(url)
 		.then(res => res.json())
 		.then(res => {
+			console.log(res.forms[0].name)
+			if (document.querySelector('.shadow')) {
+				document.querySelector('.shadow').remove()
+			}
 			const imgPkm = document.createElement('img')
 			imgPkm.src = res.sprites.front_default
 			imgPkm.classList.add('shadow')
+			imgPkm.setAttribute('height', '192')
+			imgPkm.setAttribute('width', '192')
 			form.after(imgPkm)
 		})
 		.catch(error => {
 			console.warn(error)
 		})
+})
+
+searchBtn.addEventListener('click', () => {
+	const guess = document.querySelector('#userGuess').value
+	console.log('guess:', guess)
 })
